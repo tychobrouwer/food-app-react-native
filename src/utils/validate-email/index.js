78a497d081d5108b3be11048066bda5bb0da43c4
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-09-19 21:40:16 +0200

    auto commit

--- src/utils/validate-email/index.js
+++ src/utils/validate-email/index.js
@@ -1,7 +1,8 @@
 const validateEmail = (email) => String(email)
   .toLowerCase()
   .match(
-    /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  );
+    // eslint-disable-next-line no-useless-escape
+    /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i,
+  )[0];
 
 export default validateEmail;
